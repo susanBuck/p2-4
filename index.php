28659b21d349d5031c25db923a40c88096be6779
Mal Rice <m.perryrice@gmail.com>
2018-09-26 20:26:27 -0400

    Added initial html, php, and css docs

---
+++ index.php
@@ -0,0 +1,20 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Project 2</title>
+    <link rel="stylesheet" href="css/style.css">
+    <?php include('php/logic.php'); ?>
+
+</head>
+<body>
+
+<h1> Project 2 - Mallory Rice </h1>
+
+
+</body>
+</html>
